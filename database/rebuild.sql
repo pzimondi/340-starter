e0617fced1119b1d6f495f6a3911fd114afd4978
pzimondi <pzimondi@byupathway.edu>
2026-01-18 02:15:42 +0200

    assignment2

--- database/rebuild.sql
+++ database/rebuild.sql
@@ -256,3 +256,26 @@ VALUES   (
 
 
 
+UPDATE inventory
+SET inv_description = REPLACE(
+  inv_description,
+  'small interiors',
+  'a huge interior'
+)
+WHERE inv_make = 'GM'
+AND inv_model = 'Hummer';
+
+
+
+UPDATE inventory
+SET
+  inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
+
+
+
+
+
+
